057b6ba2129c8bd350cb24aeb7a757f8a7a514f9
josedtf <josedavidtorofranco@gmail.com>
2024-04-11 21:49:51 -0500

    creacion proyecto

---
+++ Social/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def signup(request):
+    return render(request, 'signup.html')
